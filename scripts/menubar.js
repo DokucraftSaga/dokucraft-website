2e2f352ed7762eee394278a06e4b10f543173826
CCCode <eventorset@gmail.com>
2017-12-29 06:53:59 +0100

    Replaced the sidebar with a menu at the top of the page and updated some info.

--- scripts/menubar.js
+++ scripts/menubar.js
@@ -1,27 +1,4 @@
-var isMenubarOpen = false
 var isShareIconsVisible = false
-
-function closeMenubar() {
-  $("#open-menu-blocker").fadeOut(215)
-  $("#menu").css("left", "-300px")
-  $("#header").css("left", "0px").removeClass("menubarIsOpen")
-  isMenubarOpen = false
-}
-
-function openMenubar() {
-  $("#open-menu-blocker").fadeIn(215)
-  $("#menu").css("left", "0px")
-  $("#header").css("left", "300px").addClass("menubarIsOpen")
-  isMenubarOpen = true
-}
-
-function toggleMenubar() {
-  if (isMenubarOpen) {
-    closeMenubar()
-  } else {
-    openMenubar()
-  }
-}
 
 function toggleShareIcons() {
   if (!isShareIconsVisible) {
