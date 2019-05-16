14d21df26765bb506f036e8f50e3c492e4af4c73
Even Torset <eventorset@gmail.com>
2019-05-16 20:40:01 +0200

    Attempt to fix all pages leading to a 404.

--- Gruntfile.js
+++ Gruntfile.js
@@ -198,7 +198,7 @@ module.exports = function(grunt) {
         }
       },
       src: 'templates/pack.pug',
-      dest: '../dist/' + key + '.html'
+      dest: '../dist/' + key + '/index.html'
     })
   }
 
